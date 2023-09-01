be84728f414fa0f31a52dc196f041abcefe75e10
188812681 <40532397+188812681@users.noreply.github.com>
2023-09-02 00:08:24 +0800

    更新 envduqu.js

--- envduqu.js
+++ envduqu.js
@@ -2,6 +2,7 @@
 读取环境变量
 2 2 2 2 2 envduqu.js, tag=读取环境变量, enable=ture
 */
+const $ = new Env('读取环境变量');
 const notify = require('./sendNotify');
 console.log(process.env.JD_COOKIE)
 notify.sendNotify('123')
